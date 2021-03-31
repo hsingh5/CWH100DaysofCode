f579307cd7b414fd2d35c0e7d28a335596089cab
hsingh5 <tech.harjeet@gmail.com>
2021-03-31 09:36:20 -0400

    new commit

--- Tut6Day3March292021.py
+++ Tut6Day3March292021.py
@@ -2,7 +2,7 @@
 if age=18 come to office for licence
 if age>18 he can drive
 if age<18 you cantdrive
-"""
+
 agecust1=int(input("Enter the age:"))
 
 if (agecust1 < 18 or agecust1>80):
@@ -11,3 +11,15 @@ elif (agecust1>18 and agecust1<80):
     print("You can drive")
 else:
     print("Come to office for Driving License")
+"""
+
+
+#Exercise- To use the above quiz in list and use IN and NOT IN keyword
+drivage=[18,19,20,21,22,23,24,25,26,27,28,29,30]
+agecust2=int(input("Enter your age:"))
+if (agecust2 in drivage):
+    print("You can drive!")
+elif (agecust2 not in drivage):
+    print("You cannot drive!")
+elif (agecust2 < 18):
+    print("Come to office and we will evaluate")
