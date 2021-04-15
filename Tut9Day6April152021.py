0725283a8192ac95906610fe08241ae06165f6d0
hsingh5 <tech.harjeet@gmail.com>
2021-04-15 15:33:16 -0400

    Initial Commit-Learning shorthand for if else

---
+++ Tut9Day6April152021.py
@@ -0,0 +1,5 @@
+# short hand if else operator
+
+a = int(input("Enter tha value of a:"))
+b = int(input("Enter tha value of b :"))
+print("a is greater than b") if a > b else print("b is greater than a")
