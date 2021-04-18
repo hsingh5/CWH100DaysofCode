81895e2a291b7fdd87545be0a73acf1d3490712e
hsingh5 <tech.harjeet@gmail.com>
2021-04-18 19:28:12 -0400

    Initial commit

---
+++ Exercise4Day8April182021.py
@@ -0,0 +1,12 @@
+"""i=int(input("Enter the number:"))
+
+for l in reversed(range(i,0)):
+    print(l)
+
+"""
+i = int(input("Enter the number:"))
+horizontal_range = 0
+vertical_range = 0
+
+for horizontal_range in range(0, i):
+    print(vertical_range)
