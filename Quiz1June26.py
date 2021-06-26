875fee9974f29d0ee89db233bd4fd25315482468
hsingh5 <tech.harjeet@gmail.com>
2021-06-26 07:10:43 -0400

    Initial commit

---
+++ Quiz1June26.py
@@ -0,0 +1,5 @@
+"""Quiz:take a input of 2 numbers and add it"""
+var1 = int(input("Enter the first number:"))
+var2 = int(input("Enter the second number:"))
+sum = var1 + var2
+print("The sum is:\t ", sum)
--- Tut9Day6April152021.py
+++
@@ -1,5 +0,0 @@
-# short hand if else operator
-
-a = int(input("Enter tha value of a:"))
-b = int(input("Enter tha value of b :"))
-print("a is greater than b") if a > b else print("b is greater than a")
