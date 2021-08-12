339d4e7daced408f2475c401b70bdf909274b5d0
hsingh5 <tech.harjeet@gmail.com>
2021-08-12 13:21:25 -0400

    Faulty Calculator

---
+++ Quiz2CWHAug12.py
@@ -0,0 +1,14 @@
+"""
+Take input from the user as "Age"
+If age>18,user can drive
+If age<18,user connot drive
+If age=18,user has to go to RTO Office
+"""
+age_user = int(input("Enter the age:"))
+
+if age_user > 18 and age_user < 80:
+    print("You can drive!")
+elif age_user >= 1 and age_user < 18:
+    print("You are not eligible")
+else:
+    print("Come to Office for Documents")
