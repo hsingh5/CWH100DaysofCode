11617fd48e038ad26027c87dbc131b4cf673fd15
hsingh5 <tech.harjeet@gmail.com>
2021-08-18 12:57:18 -0400

    Short hand for if,else

---
+++ CWHTutorial13Aug18.py
@@ -0,0 +1,8 @@
+# Short hand for if,else
+a = int(input("Enter number 1:"))
+b = int(input("Enter number 2:"))
+
+"""if(a>b):print("A is greater bro")
+else: print("B is greater bro ")
+"""
+print("B is greater than A") if b > a else print("A is greater than B")
