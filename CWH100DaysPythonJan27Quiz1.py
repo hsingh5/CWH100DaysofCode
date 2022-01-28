a9e1f7522c25c5ced599476183bca2301deddad8
hsingh5 <tech.harjeet@gmail.com>
2022-01-27 20:59:16 -0500

    Quiz1= Enter 2 numbers as input and simply add them!

---
+++ CWH100DaysPythonJan27Quiz1.py
@@ -0,0 +1,7 @@
+"""
+Quiz:Enter 2 numbers as input and simply add them!
+"""
+var1 = int(input("Enter first number:"))
+var2 = int(input("Enter second number:"))
+sum = var1 + var2
+print("Sum is:", sum, end="")
